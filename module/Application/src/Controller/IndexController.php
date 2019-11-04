2384bd9dec7e3da5f3fd08b25ad6482c19c50123
Daniel Pérez <daniel.perez@urjc.es>
2019-11-04 17:52:43 +0100

    prueba get

--- module/Application/src/Controller/IndexController.php
+++ module/Application/src/Controller/IndexController.php
@@ -1,19 +1,39 @@
 <?php
-/**
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
+    /**
+     * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+     * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
+     * @license   http://framework.zend.com/license/new-bsd New BSD License
+     */
 
-namespace Application\Controller;
+    namespace Application\Controller;
 
-use Zend\Mvc\Controller\AbstractActionController;
-use Zend\View\Model\ViewModel;
+    use Zend\Mvc\Controller\AbstractRestfulController;
 
-class IndexController extends AbstractActionController
-{
-    public function indexAction()
+    class IndexController extends AbstractRestfulController
     {
-        return new ViewModel();
+        public function create($data)
+        {
+            $this->response->setStatusCode(200);
+            return [
+                'content' => 'prueba create'
+            ];
+        }
+
+        public function get($id)
+        {
+            $this->response->setStatusCode(200);
+            return [
+                'content' => 'prueba get'
+            ];
+        }
+
+        public function getList()
+        {
+            $this->response->setStatusCode(200);
+            $this->response->setContent(json_encode([
+                'content' => 'prueba get list'
+            ]));
+            return $this->response;
+        }
+
     }
-}
